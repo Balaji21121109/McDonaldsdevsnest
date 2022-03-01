e88d8a22f346749e4de3ec997ca1cd3e0d7f041a
Balaji A <93375333+Balaji21121109@users.noreply.github.com>
2022-03-01 12:43:04 +0530

    homepage

---
+++ homepage.js
@@ -0,0 +1,44 @@
+function myfunction2(){
+    var h1=document.getElementById("script_h1").textContent="Pizza McPuff®";
+    
+    var h2=document.getElementById("script_h2").textContent="Something different. Something delicious"
+    var p=document.getElementById("script_p").textContent="A blend of assorted vegetables (carrot, beans,capsicum, onion & green peas); mozzarella cheese mixed with tomato sauce; and exotic spices stuffed in rectangle shaped savoury dough. Quick frozen."
+    var h3=document.getElementById("script_h3").textContent="Serving Size:87 gm";
+    var h22=document.getElementById("script_h22").textContent="Allergen Warning! Contains:"
+    var pp=document.getElementById("script_pp").textContent="Gluten, Milk and/or Milk products, Soybean derivative";
+    
+    var box=document.getElementById("box_img22")
+    box.src="https://mcdindia.com/wp-content/uploads/2020/02/3.1-pizza-mcpuff.png";
+    
+    event.preventDefault();
+    }
+    function myfunction3(){
+        var h1=document.getElementById("script_h1").textContent="McFlurry (Oreo)®";
+        
+        var h2=document.getElementById("script_h2").textContent="Burgers aren't the only things we're good at."
+        var p=document.getElementById("script_p").textContent="Milk-based frozen dessert with oreo cookies"
+        var h3=document.getElementById("script_h3").textContent="Serving Size: 87 gm | 147 gm";
+        var h22=document.getElementById("script_h22").textContent="Allergen Warning! Contains:"
+        var pp=document.getElementById("script_pp").textContent="Gluten, Milk and/or Milk products";
+        
+        var box=document.getElementById("box_img22")
+        box.src="https://mcdindia.com/wp-content/uploads/2020/02/4.1-mcflurry-oreo.png";
+        
+        event.preventDefault();
+        }
+    
+        function myfunction4(){
+            var h1=document.getElementById("script_h1").textContent="Black Coffee®";
+            
+            var h2=document.getElementById("script_h2").textContent="Perfectly brewed for any time of the day."
+            var p=document.getElementById("script_p").textContent="A blend of assorted vegetables (carrot, beans,capsicum, onion & green peas); mozzarella cheese mixed with tomato sauce; and exotic spices stuffed in rectangle shaped savoury dough. Quick frozen."
+            var h3=document.getElementById("script_h3").textContent="Serving Size:200ml";
+            var h22=document.getElementById("script_h22").textContent="Note:"
+            var pp=document.getElementById("script_pp").textContent="Milk tubs and sugar sachets available separately";
+            
+            var box=document.getElementById("box_img22")
+            box.src="https://mcdindia.com/wp-content/uploads/2020/03/5.1-coffee-large-1.png";
+            
+            event.preventDefault();
+            }
+    
